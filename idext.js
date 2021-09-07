9a8996733a0d3aaa99171abfca5bf83cd788f172
JULIOTON12 <89815401+JULIOTON12@users.noreply.github.com>
2021-09-07 18:44:49 -0500

    Update idext.js

--- idext.js
+++ idext.js
@@ -1,5 +1,24 @@
-function getIMC(){ 
-var peso=parseFloat(document.getElementById('peso').value);
-var altura=parseFloat(document.getElementById('altura').value);
-var imc=peso/(altura*altura);
-document.getElementById("imc").innerHTML=imc;}
+<script type="text/javascript">
+	calc = document.getElementById("calc");
+	kg = document.getElementById("kg");
+	m = document.getElementById("m");
+	imc = document.getElementById("imc");
+	lectura = document.getElementById("lectura");
+
+	calc.onclick = function(){
+		if(kg.value!="" && m.value!=""){
+			imcx = (kg.value / (m.value* m.value));
+			imc.innerHTML = imcx
+			console.log(imcx);
+
+			if(imcx<18.5){ lectura.innerHTML = "Peso inferior al normal"; }
+			else if(imcx>=18.5 && imcx<=24.9){ lectura.innerHTML = "Peso normal"; }
+			else if(imcx>=25 && imcx<=29.9){ lectura.innerHTML = "Peso superior al normal"; }
+			else if(imcx>30){ lectura.innerHTML = "Obesidad"; }
+
+		}else{
+			alert("Debes ingresar peso y altura.")
+		}
+
+	};
+</script>
